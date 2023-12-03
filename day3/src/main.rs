8e19540bdef558fb008ec5f64ea3f22918205271
Callum Collins <callum_collins@hotmail.com>
2023-12-03 14:05:11 +0000

    Refactored both solutions

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -15,254 +15,143 @@ fn main() {
         println!("Done in: {:.2?}", now.elapsed());
     }
 }
-#[allow(clippy::too_many_lines)]
-fn day3(lines: &[String]) -> i32 {
-    let numpos = lines
+
+struct NumPos {
+    column: i32,
+    length: i32,
+    num: i32,
+}
+
+fn get_num_positions(lines: &[String]) -> Vec<Vec<NumPos>> {
+    let num_pos = lines
         .iter()
-        .enumerate()
-        .map(|(index, line)| {
+        .map(|line| {
             let mut num = String::new();
-            let mut nums: Vec<((i32, i32, i32), i32)> = vec![];
+            let mut nums: Vec<NumPos> = vec![];
             let _ = line
                 .chars()
                 .enumerate()
-                .map(|(i, c)| {match c.to_string().parse::<i32>() {
-                    Ok(c) => num += &c.to_string(),
-                    Err(_) => {
-                        if !num.is_empty() {
-                            nums.push((
-                                (
-                                    index.try_into().unwrap(),
-                                    (i - num.len()).try_into().unwrap(),
-                                    num.len().try_into().unwrap(),
-                                ),
-                                num.parse::<i32>().unwrap(),
-                            ));
-                            num = String::new();
+                .map(|(i, c)| {
+                    match c.to_string().parse::<i32>() {
+                        Ok(c) => num += &c.to_string(),
+                        Err(_) => {
+                            if !num.is_empty() {
+                                nums.push(NumPos {
+                                    column: (i - num.len()).try_into().unwrap(),
+                                    length: num.len().try_into().unwrap(),
+                                    num: num.parse::<i32>().unwrap(),
+                                });
+                                num = String::new();
+                            }
                         }
-                    }};
-                    
-                    if i == line.len()-1 && !num.is_empty() {
-                        nums.push((
-                            (
-                                index.try_into().unwrap(),
-                                (i - num.len() + 1).try_into().unwrap(),
-                                num.len().try_into().unwrap(),
-                            ),
-                            num.parse::<i32>().unwrap()));
+                    };
+
+                    if i == line.len() - 1 && !num.is_empty() {
+                        nums.push(NumPos {
+                            column: (i - num.len() + 1).try_into().unwrap(),
+                            length: num.len().try_into().unwrap(),
+                            num: num.parse::<i32>().unwrap(),
+                        });
                     }
-                
                 })
                 .collect::<Vec<_>>();
             nums
         })
         .collect::<Vec<_>>();
+    num_pos
+}
+
+fn find_nums(numpos: &[Vec<NumPos>], index: usize, i: i32) -> Vec<i32> {
+    numpos[index]
+        .iter()
+        .filter(|c| (i - c.column).abs() <= 1 || i >= c.column && i <= c.column + c.length)
+        .map(|x| x.num)
+        .collect::<Vec<_>>()
+}
+
+fn day3(lines: &[String]) -> i32 {
+    let numpos = get_num_positions(lines);
 
     lines
         .iter()
         .enumerate()
         .map(|(index, line)| {
-            line
-                .chars()
+            line.chars()
                 .enumerate()
                 .map(|(ix, ch)| {
                     let i: i32 = ix.try_into().unwrap();
                     match ch {
                         '*' | '#' | '$' | '+' | '%' | '=' | '-' | '&' | '@' | '/' => {
+                            let a:i32 = find_nums(&numpos, index, i).iter().sum();
                             if index == 0 {
-                                let b:i32 = numpos[index]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    .collect::<Vec<_>>().iter().sum();
-                                let c:i32 = numpos[index + 1]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    .collect::<Vec<_>>().iter().sum();
-                               b + c
+                                let c: i32 = find_nums(&numpos, index + 1, i).iter().sum();
+                                a + c
                             } else if index == lines.len() - 1 {
-                                let b:i32 = numpos[index]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    .collect::<Vec<_>>().iter().sum();
-                                let c:i32 = numpos[index  - 1]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    .collect::<Vec<_>>().iter().sum();
-                                b + c
+                                let c: i32 = find_nums(&numpos, index - 1, i).iter().sum();
+                                a + c
                             } else {
-                                let a:i32 = numpos[index - 1]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    .collect::<Vec<_>>().iter().sum();
-                                let b:i32 = numpos[index]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    .collect::<Vec<_>>().iter().sum();
-                                let c:i32 = numpos[index + 1]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    .collect::<Vec<_>>().iter().sum();
+                                let b: i32 = find_nums(&numpos, index - 1, i).iter().sum();
+                                let c: i32 = find_nums(&numpos, index + 1, i).iter().sum();
                                 a + b + c
                             }
                         }
                         _ => 0,
                     }
                 })
-                .collect::<Vec<_>>().iter().sum()
+                .collect::<Vec<_>>()
+                .iter()
+                .sum()
         })
-        .collect::<Vec<_>>().iter().sum()
+        .collect::<Vec<_>>()
+        .iter()
+        .sum()
 }
 
-
-#[allow(clippy::too_many_lines)]
 fn day3part2(lines: &[String]) -> i32 {
-    let numpos = lines
-        .iter()
-        .enumerate()
-        .map(|(index, line)| {
-            let mut num = String::new();
-            let mut nums: Vec<((i32, i32, i32), i32)> = vec![];
-            let _ = line
-                .chars()
-                .enumerate()
-                .map(|(i, c)| {match c.to_string().parse::<i32>() {
-                    Ok(c) => num += &c.to_string(),
-                    Err(_) => {
-                        if !num.is_empty() {
-                            nums.push((
-                                (
-                                    index.try_into().unwrap(),
-                                    (i - num.len()).try_into().unwrap(),
-                                    num.len().try_into().unwrap(),
-                                ),
-                                num.parse::<i32>().unwrap(),
-                            ));
-                            num = String::new();
-                        }
-                    }};
-                    
-                    if i == line.len()-1 && !num.is_empty() {
-                        nums.push((
-                            (
-                                index.try_into().unwrap(),
-                                (i - num.len() + 1).try_into().unwrap(),
-                                num.len().try_into().unwrap(),
-                            ),
-                            num.parse::<i32>().unwrap()));
-                    }
-                
-                })
-                .collect::<Vec<_>>();
-            nums
-        })
-        .collect::<Vec<_>>();
+    let numpos = get_num_positions(lines);
 
     lines
         .iter()
         .enumerate()
         .map(|(index, line)| {
-            line
-                .chars()
+            line.chars()
                 .enumerate()
                 .map(|(ix, ch)| {
                     let i: i32 = ix.try_into().unwrap();
                     match ch {
-                        '*'  => {
+                        '*' => {
+                            let mut a: Vec<i32> = find_nums(&numpos, index, i);
                             if index == 0 {
-                                let mut a = numpos[index]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    .collect::<Vec<_>>();
-                                let mut b = numpos[index + 1]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    
-                                    .collect::<Vec<_>>();
+                                let mut b = find_nums(&numpos, index + 1, i);
                                 a.append(&mut b);
-                                a.retain(|x: &i32| *x != 0);
 
-                                if ch == '*' && a.len() == 2 {
-                                    a[0] * a[1]
-                                } else {
-                                    a.iter().sum()
-                                }
                             } else if index == lines.len() - 1 {
-                                let mut b = numpos[index]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    
-                                    .collect::<Vec<_>>();
-                                let mut c = numpos[index  - 1]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    .collect::<Vec<_>>();
-                                b.append(&mut c);
-                                b.retain(|x: &i32| *x != 0);
-
-                                if ch == '*' && b.len() == 2 {
-                                    b[0] * b[1]
-                                } else {
-                                    b.iter().sum()
-                                }
+                                let mut c = find_nums(&numpos, index - 1, i);
+                                a.append(&mut c);
                             } else {
-                                let mut a = numpos[index - 1]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    .collect::<Vec<_>>();
-                                let mut b = numpos[index]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    
-                                    .collect::<Vec<_>>();
-                                let mut c = numpos[index + 1]
-                                    .iter()
-                                    .filter(|c| {
-                                        (i - c.0 .1).abs() <= 1 || i >= c.0 .1 && i <= c.0 .1 + c.0 .2
-                                    }).map(| x | x.1)
-                                    
-                                    .collect::<Vec<_>>();
+                                let mut b = find_nums(&numpos, index - 1, i);
+                                let mut c = find_nums(&numpos, index + 1, i);
                                 a.append(&mut b);
                                 a.append(&mut c);
-                                a.retain(|x: &i32| *x != 0);
-                                if ch == '*' && a.len() == 2 {
-                                    a[0] * a[1]
-                                } else {
-                                    0
-                                }
+
+                            }
+                            a.retain(|x: &i32| *x != 0);
+                            if ch == '*' && a.len() == 2 {
+                                a[0] * a[1]
+                            } else {
+                                0
                             }
                         }
                         _ => 0,
                     }
                 })
-                .collect::<Vec<_>>().iter().sum()
+                .collect::<Vec<_>>()
+                .iter()
+                .sum()
         })
-        .collect::<Vec<_>>().iter().sum()
+        .collect::<Vec<_>>()
+        .iter()
+        .sum()
 }
 
 #[cfg(test)]
@@ -290,7 +179,7 @@ mod tests {
     }
     #[test]
     fn same_line2() {
-        let i = "467.114+22\n\
+        let i = "467..114+22\n\
                        ..........";
         let lines: Vec<String> = i.lines().map(std::string::ToString::to_string).collect();
         assert_eq!(day3(&lines), 136);
@@ -312,7 +201,7 @@ mod tests {
     }
     #[test]
     fn prev_line() {
-        let i = "467.114.22\n\
+        let i = "467..114.22\n\
                        .....+....\n";
         let lines: Vec<String> = i.lines().map(std::string::ToString::to_string).collect();
         assert_eq!(day3(&lines), 114);
@@ -359,7 +248,7 @@ mod tests {
 
     #[test]
     fn mid_line() {
-        let i = "......+..\n\
+        let i = ".......+..\n\
                        ...12.345.\n\
                        ...+......\n";
         let lines: Vec<String> = i.lines().map(std::string::ToString::to_string).collect();
@@ -368,11 +257,10 @@ mod tests {
 
     #[test]
     fn mid_line2() {
-        let i = ".....12...\n\
-                       ...+..+...\n\
-                       ..123.....\n";
+        let i = "......12...\n\
+                       ...+...+...\n\
+                       ...123.....\n";
         let lines: Vec<String> = i.lines().map(std::string::ToString::to_string).collect();
         assert_eq!(day3(&lines), 135);
     }
-
 }
