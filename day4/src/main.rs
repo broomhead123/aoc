5169ad02ec650d7d15ee5527969f28ede7641b45
Callum Collins <callum_collins@hotmail.com>
2023-12-04 20:09:54 +0000

    Day4 done pre refactor

---
+++ day4/src/main.rs
@@ -0,0 +1,168 @@
+use std::time::Instant;
+use std::collections::HashSet;
+use std::cmp::min;
+fn main() {
+    let file = include_str!("../input.data");
+    let lines: Vec<String> = file.lines().map(std::string::ToString::to_string).collect();
+    {
+        let now = Instant::now();
+        let total_part1: i64 = day4_hash(&lines);
+        println!("Part 1 {total_part1}");
+        println!("Done in: {:.2?}", now.elapsed());
+    }
+    {
+        let now = Instant::now();
+        let total_part1: i64 = day4(&lines);
+        println!("Part 1b {total_part1}");
+        println!("Done in: {:.2?}", now.elapsed());
+    }
+    {
+        let now = Instant::now();
+        let total_part2 = day4part2(&lines, 10);
+        println!("Part 2 {total_part2}");
+        println!("Done in: {:.2?}", now.elapsed());
+    }
+}
+
+fn day4(lines: &[String]) -> i64 {
+    lines
+        .iter()
+        .map(|line| {
+            let binding = line.split(':').collect::<Vec<_>>();
+            let l = binding.last().unwrap();
+            let winning_numbers = l
+                .get(1..30)
+                .unwrap()
+                .split(' ')
+                .filter(|x| !x.is_empty())
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
+
+            let num_to_check = l
+                .get(33..)
+                .unwrap()
+                .split(' ')
+                .filter(|x| !x.is_empty())
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
+
+            
+            let num_wins: i64 = num_to_check
+                .iter()
+                .filter(|n| winning_numbers.contains(n))
+                .count()
+                .try_into()
+                .unwrap();
+            if num_wins > 0 {
+                2_i64.pow(num_wins as u32 -1)
+            } else {
+                0
+            }
+        })
+        .collect::<Vec<_>>()
+        .iter()
+        .sum()
+}
+
+
+fn day4_hash(lines: &[String]) -> i64 {
+    lines
+        .iter()
+        .map(|line| {
+            let binding = line.split(':').collect::<Vec<_>>();
+            let l = binding.last().unwrap();
+
+            let winning_numbers: HashSet<i32> = l
+                .get(1..30)
+                .unwrap()
+                .split(' ')
+                .filter(|x| !x.is_empty())
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect::<HashSet<_>>();
+
+            let num_to_check: HashSet<i32> = l
+                .get(33..)
+                .unwrap()
+                .split(' ')
+                .filter(|x| !x.is_empty())
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect::<HashSet<_>>();
+            let num_wins: i64 = i64::try_from(winning_numbers.intersection(&num_to_check).count()).unwrap();
+            if num_wins > 0 {
+                2_i64.pow(num_wins as u32 -1)
+            } else {
+                0
+            }
+        })
+        .collect::<Vec<_>>()
+        .iter()
+        .sum()
+}
+
+
+
+#[derive(Debug, Clone)]
+struct ScratchCard {
+    matches: i64,
+    copies: i64
+}
+
+fn day4part2(lines: &[String], win_nums: usize) -> i64 {
+    let mut scratch_cards: Vec<ScratchCard> = lines
+        .iter()
+        .map(|line| {
+            let binding = line.split(':').collect::<Vec<_>>();
+            let l = binding.last().unwrap();
+            let winning_numbers = l
+                .get(1.. win_nums * 3)
+                .unwrap()
+                .split(' ')
+                .filter(|x| !x.is_empty())
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
+
+            let num_to_check = l
+                .get(((win_nums * 3) + 3)..)
+                .unwrap()
+                .split(' ')
+                .filter(|x| !x.is_empty())
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
+            
+            let num_wins: i64 = num_to_check
+                .iter()
+                .filter(|n| winning_numbers.contains(n))
+                .count()
+                .try_into()
+                .unwrap();
+
+            ScratchCard{
+                matches:num_wins,
+                copies:1
+            }
+            
+        })
+        .collect::<Vec<_>>();
+    let len = scratch_cards.len();
+    for i in 0 .. scratch_cards.len()  {
+        let card = scratch_cards[i].clone();
+        if card.matches != 0 {
+            for s in scratch_cards[i+1 ..=min(i + card.matches as usize, len)].iter_mut() {
+                s.copies += card.copies;
+            }
+        }
+    }
+    scratch_cards.iter().map(|s| s.copies).collect::<Vec<_>>().iter().sum::<i64>()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn part2() {
+        let file = include_str!("../test.data");
+        let lines: Vec<String> = file.lines().map(std::string::ToString::to_string).collect();
+        assert_eq!(day4part2(&lines, 5), 30);
+    }
+}
