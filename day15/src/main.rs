e31246471fe50af0f4dc466850278036716ffa38
Callum Collins <callum_collins@hotmail.com>
2023-12-15 11:54:31 +0000

    Day 15 tidy

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -18,18 +18,7 @@ fn main() {
 }
 
 fn day14(lines: &[String]) -> i64 {
-    lines[0]
-        .split(',')
-        .map(|s| {
-            let mut hash: i64 = 0;
-            for c in s.chars() {
-                hash += c as i64;
-                hash *= 17;
-                hash %= 256;
-            }
-            hash
-        })
-        .sum()
+    lines[0].split(',').map(hash_str).sum()
 }
 
 fn day14part2(lines: &[String]) -> i64 {
@@ -37,39 +26,23 @@ fn day14part2(lines: &[String]) -> i64 {
     let _ = lines[0]
         .split(',')
         .map(|s| {
-            let mut hash: i64 = 0;
-            let label = s.split(|c| c == '-' || c == '=').collect::<Vec<_>>()[0];
-            let focal_length = if s.contains("=") {
-                s.split(|c| c == '-' || c == '=').collect::<Vec<_>>()[1]
-                    .parse::<i64>()
-                    .unwrap()
-            } else {
-                -1
-            };
-            for c in label.chars() {
-                hash += c as i64;
-                hash *= 17;
-                hash %= 256;
-            }
-            if focal_length == -1 {
-                if boxes[hash as usize].iter().any(|x| x.0 == label) {
-                    boxes[hash as usize].remove(
-                        boxes[hash as usize]
-                            .iter()
-                            .position(|x| x.0 == label)
-                            .unwrap(),
-                    );
-                    boxes[hash as usize].retain(|x| x.0 != "");
+            let split = s.split(|c| c == '-' || c == '=').collect::<Vec<_>>();
+            let label = split[0];
+            let focal_length = split[1].parse::<i64>();
+
+            let hash = usize::try_from(hash_str(label)).unwrap();
+            let exists = boxes[hash as usize].iter().any(|x| x.0 == label);
+            let pos = boxes[hash].iter().position(|x| x.0 == label);
+            if focal_length.is_err() {
+                if exists {
+                    boxes[hash].remove(pos.unwrap());
+                    boxes[hash as usize].retain(|x| !x.0.is_empty());
                 }
-            } else if boxes[hash as usize].iter().any(|x| x.0 == label) {
-                let pos = boxes[hash as usize]
-                    .iter()
-                    .position(|x| x.0 == label)
-                    .unwrap();
-                //boxes[hash as usize].remove(pos);
-                boxes[hash as usize][pos] = (label, focal_length);
+            } else if exists {
+                let pos = pos.unwrap();
+                boxes[hash][pos] = (label, focal_length.unwrap());
             } else {
-                boxes[hash as usize].push((label, focal_length));
+                boxes[hash].push((label, focal_length.unwrap()));
             }
         })
         .collect::<Vec<()>>();
@@ -80,12 +53,22 @@ fn day14part2(lines: &[String]) -> i64 {
             .map(|b| {
                 b.1.iter()
                     .enumerate()
-                    .map(|l| (b.0 + 1) * (l.0 + 1) * (l.1 .1 as usize))
+                    .map(|l| (b.0 + 1) * (l.0 + 1) * usize::try_from(l.1 .1).unwrap())
                     .sum::<usize>()
             })
             .sum::<usize>(),
     )
     .unwrap()
+}
+
+fn hash_str(label: &str) -> i64 {
+    let mut hash = 0;
+    for c in label.chars() {
+        hash += c as i64;
+        hash *= 17;
+        hash %= 256;
+    }
+    hash
 }
 
 #[cfg(test)]
@@ -94,9 +77,9 @@ mod tests {
 
     #[test]
     fn part1hash() {
-        let file = include_str!("../test.data");
-        assert_eq!(day14(&["HASH".to_string()]), 52);
+        assert_eq!(hash_str("HASH"), 52);
     }
+
     #[test]
     fn part1() {
         let file = include_str!("../test.data");
